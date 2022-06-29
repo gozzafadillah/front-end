6699cf27a157fe7c187f916de554f8b43f5dbca5
Nanda Hady Mulya <nandahadymulyaa@gmail.com>
2022-06-30 04:44:50 +0700

    feat: updated dashboard card

--- components/DashboardCard.vue
+++ components/DashboardCard.vue
@@ -1,21 +1,23 @@
 <template>
-  <v-container>
-    <v-card class="flat card" v-bind="$attrs" v-on="$listeners">
-      <v-row wrap>
-        <v-col cols="3" align="center" align-self="center">
-          <v-icon class="justify-center" :color="colorIcon" x-large>
-            {{ icon }}
-          </v-icon>
-        </v-col>
-        <v-col cols="9">
-          <v-card-title class="ma-0 pa-0"> {{ title }} </v-card-title>
-          <v-card-text class="text-h4 ma-0 pa-0 font-weight-bold">
-            {{ count }}
-          </v-card-text>
-        </v-col>
-      </v-row>
-    </v-card>
-  </v-container>
+  <v-card
+    :class="[dark ? 'dark' : 'light', 'flat', 'card']"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <v-row wrap>
+      <v-col cols="3" align="center" align-self="center">
+        <v-icon :color="colorIcon" x-large>
+          {{ icon }}
+        </v-icon>
+      </v-col>
+      <v-col cols="9">
+        <v-card-title class="ma-0 pa-0"> {{ title }} </v-card-title>
+        <v-card-text class="text-h4 ma-0 pa-0 font-weight-bold">
+          {{ count }}
+        </v-card-text>
+      </v-col>
+    </v-row>
+  </v-card>
 </template>
 <script>
 export default {
@@ -34,6 +36,10 @@ export default {
       type: Number,
       default: 0,
     },
+    dark: {
+      type: Boolean,
+      default: false,
+    },
     colorIcon: {
       type: String,
       default: '#0092E4',
@@ -42,6 +48,14 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+.dark {
+  background-color: $bayeue-primary !important;
+  color: #fff;
+}
+.light {
+  background-color: #fff;
+  color: $bayeue-dark;
+}
 .card {
   padding: 20px 10px;
   background: #fdfdfd;
