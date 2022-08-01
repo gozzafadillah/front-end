6f6e65cb8b56a86c73097846a265f2c196f6bdd0
gozzafadillah <74459029+gozzafadillah@users.noreply.github.com>
2022-08-02 04:30:16 +0700

    try to change api fly.io

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,7 +53,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://18.136.193.234:19000/',
+    baseURL: 'https://bayeue.fly.dev/',
   },
 
   toast: {
