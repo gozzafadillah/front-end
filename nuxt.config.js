60f2bd9238250dd4a8052e3b3c43a1bd06ab999d
Muhammad Fadillah Abdul Aziz <74459029+gozzafadillah@users.noreply.github.com>
2023-08-12 23:42:42 +0700

    change api to railway

--- nuxt.config.js
+++ nuxt.config.js
@@ -53,7 +53,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://bayeue.fly.dev/',
+    baseURL: 'https://back-end-production-a56f.up.railway.app/',
   },
 
   toast: {
